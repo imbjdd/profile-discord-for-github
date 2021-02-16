5af9dde53959cb74f3dc8f19ce27e94a90494ad3
Bobbie <b0bb1e@protonmail.com>
2021-02-16 14:58:28 +0100

    update getAccessToken() add getRefreshToken() add getUserInfo() add getRefreshTokenLocal() add /api/exchange_code endpoint

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,16 @@ import fs from 'fs'
 import mustache from 'mustache'
 import axios from 'axios'
 
-const REFRESH_TOKEN_URL = 'https://discord.com/api/v8/oauth2/token'
+const GET_TOKEN_URL = 'https://discord.com/api/v8/oauth2/token'
 const INFO_TOKEN_URL = 'https://discord.com/api/v8/users/@me'
 const REDIRECT_URI = 'http://localhost:1234/callback/'
 
-async function getRefreshToken() {
+async function getAccessToken(REFRESH_TOKEN: string) {
   const params = new URLSearchParams()
   params.append('client_id', process.env.CLIENT_ID)
   params.append('client_secret', process.env.CLIENT_SECRET)
   params.append('grant_type', 'refresh_token')
-  params.append('refresh_token', process.env.REFRESH_TOKEN)
+  params.append('refresh_token', REFRESH_TOKEN)
   params.append('redirect_uri', REDIRECT_URI)
   params.append('scope', 'identify')
 
@@ -29,53 +29,89 @@ async function getRefreshToken() {
   }
 
   try {
-    const response = await axios.post(REFRESH_TOKEN_URL, params, config)
+    const response = await axios.post(GET_TOKEN_URL, params, config)
+    fs.writeFileSync("REFRESH_TOKEN", response.data.refresh_token)
     return response.data.access_token
+  } catch (e) {
+    // tslint:disable-next-line:no-console
+    console.log(e.response.data)
+    return e
+  }
+}
+
+async function getRefreshToken(code: string) {
+  const params = new URLSearchParams()
+  params.append('client_id', process.env.CLIENT_ID)
+  params.append('client_secret', process.env.CLIENT_SECRET)
+  params.append('grant_type', 'authorization_code')
+  params.append('code', code)
+  params.append('redirect_uri', REDIRECT_URI)
+  params.append('scope', 'identify')
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+
+  try {
+    const response = await axios.post(GET_TOKEN_URL, params, config)
+    return response.data
   } catch (e) {
     return e
   }
 }
 
 async function getUserInfo(ACCESS_TOKEN: string) {
-  const url = INFO_TOKEN_URL + '/users/@me'
-
   const config = {
     headers : {
-      Authorization: "Bearer " + ACCESS_TOKEN
+      Authorization: 'Bearer ' + ACCESS_TOKEN
     }
   }
 
   try {
-    const req = await axios.get(url, config)
+    const req = await axios.get(INFO_TOKEN_URL, config)
     return req.data
   } catch (e) {
+    // tslint:disable-next-line:no-console
+    console.log(e.response.data)
     return e
   }
+}
+
+function getRefreshTokenLocal() {
+  return fs.readFileSync('REFRESH_TOKEN','utf8')
+}
+
+function getSvg(view: object) {
+  const text = fs.readFileSync('views/profile.mustache','utf8')
+  return mustache.render(text, view)
 }
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send("Hello world!")
+  res.send('Hello world!')
 })
 
-app.get('/api/profile', (req, res) => {
-  const view = {
-    username: 'Joe',
-    tag: '1932',
-    id: '796889023880495105',
-    avatar: '230a6cf9e2d75857050f815731054cf2'
-  };
-
-  const text = fs.readFileSync('views/profile.mustache','utf8')
-  const output = mustache.render(text, view)
+app.get('/api/profile', async (req, res) => {
+  const REFRESH_TOKEN = await getRefreshTokenLocal()
+  const ACCESS_TOKEN = await getAccessToken(REFRESH_TOKEN.trim())
+  const USER_INFO = await getUserInfo(ACCESS_TOKEN)
+  const AWESOME_SVG = await getSvg(USER_INFO)
 
   res.writeHead(200, {
    'Content-Type': 'image/svg+xml',
-   'Content-Length': output.length
+   'Content-Length': AWESOME_SVG.length
   })
 
-  res.end(output)
+  res.end(AWESOME_SVG)
+})
+
+app.get('/api/exchange_code', async (req, res) => {
+  const code: string = req.query.code.toString()
+  const data = await getRefreshToken(code)
+  res.send(data)
 })
 
 app.listen(port, () => {
